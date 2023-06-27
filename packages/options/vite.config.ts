1f4d1a5e36cf7d2bcdb7969c61c2c4d651db7c4b
yongtay <572194404@qq.com>
2023-06-27 09:54:55 +0800

    init

--- packages/options/vite.config.ts
+++ packages/options/vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: 'packages/options/dist/',
+  plugins: [
+    react(),
+    cssInjectedByJsPlugin()
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'index.js',
+        manualChunks: () => 'index'
+      }
+    }
+  }
 })
--- packages/webpage/src/main.tsx
+++ packages/webpage/src/main.tsx
@@ -3,8 +3,28 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+if(import.meta.env.MODE === 'development') {
+  ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  )
+} else {
+  const div = document.createElement('div')
+  div.id = 'spotlight-extensions-root'
+  const props = {
+    position: 'fixed',
+    top: 0,
+    left: 0
+  }
+  for(const k in props) {
+    div.style[k] = props[k]
+  }
+  window.document.appendChild(div)
+  ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  )
+}
+
